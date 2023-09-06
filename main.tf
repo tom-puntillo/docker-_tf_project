69bea3fdf4ff8ce4a7e92943edecb2a91fa01cb9
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-09-06 16:30:52 -0400

    added ec2 module (#4)

--- main.tf
+++ main.tf
@@ -12,3 +12,15 @@ module "security_groups" {
   # Pass the VPC ID from the "vpc" module as an input
   vpc_id = module.vpc.vpc_id
 }
+
+# Declare a module named "ec2_web_servers" sourced from the "./ec2" directory
+module "ec2_docker_ubuntu" {
+  source = "./ec2"
+
+  # Pass the IDs of public subnets from the "vpc" module as inputs
+  public_subnet_1 = module.vpc.public_subnet_1_id
+  public_subnet_2 = module.vpc.public_subnet_2_id
+
+  # Pass security groups for HTTP and TLS from the "security_groups" module
+  security_groups = [module.security_groups.security_group_http, module.security_groups.security_group_tls]
+}
