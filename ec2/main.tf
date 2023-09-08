60ceb953ed6a89193381b93a8216dcc7554a36b5
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-09-08 10:41:10 -0400

    reduced ec2 count back to 3 for easier swarm management (#6)

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,7 +3,7 @@
 # Define AWS instances for web servers
 resource "aws_instance" "web_server_1" {
   ami                    = var.ami # Use the specified AMI for the instance
-  count                  = 4
+  count                  = 3
   instance_type          = var.instance_type   # Set the instance type based on the variable
   subnet_id              = var.public_subnet_1 # Use the first public subnet from the variable
   user_data              = var.user_data       # Use the user data defined in the variable
