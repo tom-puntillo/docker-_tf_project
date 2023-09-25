cbe3557608afb0fc4515df0d61a7ebcc2daa8547
tom-puntillo <tom.puntillo@gmail.com>
2023-09-25 21:20:34 +0000

    added jenkins security group to output file and passed it to main.tf

--- security_groups/outputs.tf
+++ security_groups/outputs.tf
@@ -13,3 +13,7 @@ output "security_group_tls" {
 output "security_group_docker" {
   value = aws_security_group.docker_cluster.id # Access the ID of the "docker_cluster" security group
 }
+
+output "security_group_jenkins" {
+  value = aws_security_group.jenkins_port.id
+}
