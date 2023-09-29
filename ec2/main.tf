a35d21575664847db006095dbd4d377b69c3dd0c
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-09-28 22:38:12 -0400

    added ec2 resource and modified another (#13)

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,11 +1,27 @@
 #---ec2-main.tf---
 
 # Define AWS instances for web servers
-resource "aws_instance" "web_server_1" {
-  ami                    = var.ami # Use the specified AMI for the instance
-  count                  = 3
+resource "aws_instance" "docker_manager_nodes" {
+  ami                    = var.ami             # Use the specified AMI for the instance
   instance_type          = var.instance_type   # Set the instance type based on the variable
   subnet_id              = var.public_subnet_1 # Use the first public subnet from the variable
   user_data              = var.user_data       # Use the user data defined in the variable
   vpc_security_group_ids = var.security_groups # Attach security groups from the variable
+
+  tags = {
+    Name = "docker_manager"
+  }
+}
+
+resource "aws_instance" "docker_worker_nodes" {
+  ami                    = var.ami # Use the specified AMI for the instance
+  count                  = 2
+  instance_type          = var.instance_type   # Set the instance type based on the variable
+  subnet_id              = var.public_subnet_1 # Use the first public subnet from the variable
+  user_data              = var.user_data       # Use the user data defined in the variable
+  vpc_security_group_ids = var.security_groups # Attach security groups from the variable
+
+  tags = {
+    Name = "docker_worker"
+  }
 }
