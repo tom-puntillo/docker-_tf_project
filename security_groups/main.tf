e88ef026d5de2ba69c741b60518aacafaa43582a
tom-puntillo <tom.puntillo@gmail.com>
2023-09-29 23:32:47 +0000

    reverted ssh to allow from anywhere

--- security_groups/main.tf
+++ security_groups/main.tf
@@ -15,11 +15,11 @@ resource "aws_security_group" "allow_http" {
   }
 
   ingress {
-    description = "SSH from My PC"
+    description = "SSH from Anywhere"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["73.0.169.120/32"] # Allow SSH traffic from any IP address
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH traffic from any IP address
   }
 
   egress {
