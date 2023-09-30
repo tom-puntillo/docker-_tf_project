4b6e9001db34786754b1f336bb2222e5970eaad5
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-09-29 20:29:39 -0400

    modified ec2 user data scripts (#16)

--- ec2/manager_user_data.sh
+++ ec2/manager_user_data.sh
@@ -3,5 +3,9 @@
 apt update -y
 apt upgrade -y
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+sh get-docker.sh
+usermod -aG sudo ubuntu
+usermod -aG adm ubuntu
+usermod -aG docker ubuntu
 git clone https://github.com/tom-puntillo/docker_compose_files.git
+mv /docker_compose_files ~/
