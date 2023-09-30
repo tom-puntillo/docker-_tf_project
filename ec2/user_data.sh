5ee92691c34c2a83eb432ebc6e16fda1e139405a
tom-puntillo <tom.puntillo@gmail.com>
2023-09-30 00:29:14 +0000

    modified ec2 user data scripts

--- ec2/user_data.sh
+++ ec2/user_data.sh
@@ -3,4 +3,7 @@
 apt update -y
 apt upgrade -y
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+sh get-docker.sh
+usermod -aG sudo ubuntu
+usermod -aG adm ubuntu
+usermod-aG docker ubuntu
