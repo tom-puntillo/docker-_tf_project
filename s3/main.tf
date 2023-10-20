27eb1bd9f0dbd74a6ee5a66f7322decb994c4b38
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-10-20 14:34:03 -0400

    corrected s3 bucket name (#25)

--- s3/main.tf
+++ s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tommy_docker_tf_bucket" {
-  bucket = "tommy_docker_tf_bucket_158646513521"
+  bucket = "tommy-docker-tf-bucket-158646513521"
 
   tags = {
     Name = "Docker bucket"
