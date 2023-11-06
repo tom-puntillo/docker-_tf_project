9a17fe917a43f0d076ba02003a126f3fc7ddb015
tom-puntillo <tom.puntillo@gmail.com>
2023-11-06 20:36:22 +0000

    added an ec2 worker node

--- ec2/main.tf
+++ ec2/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "docker_manager_nodes" {
 
 resource "aws_instance" "docker_worker_nodes" {
   ami                    = var.ami # Use the specified AMI for the instance
-  count                  = 2
+  count                  = 4
   instance_type          = var.instance_type   # Set the instance type based on the variable
   subnet_id              = var.public_subnet_1 # Use the first public subnet from the variable
   user_data              = var.user_data       # Use the user data defined in the variable
